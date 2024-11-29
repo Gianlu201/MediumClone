e51518bed463ec956ef6e5f80c389a7bc71cdfd7
Gianlu201 <gianlucadidiego2001@gmail.com>
2024-11-29 16:49:17 +0100

    effect sfarfallio M done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('header');
 const heroSect = document.getElementById('hero');
 const getStarted = document.querySelector('nav li.start')
+const m = document.querySelectorAll('#mGrid path');
+
 
 document.addEventListener('scroll', function(event) {
   if (window.scrollY >= heroSect.offsetHeight -30 ) {
@@ -11,3 +13,28 @@ document.addEventListener('scroll', function(event) {
     getStarted.classList.remove('green');
   }
 })
+
+const ma = m.length;
+
+function sfarfallio() {
+  for (let i = 0; i<20; i++) {
+    let j = Math.floor(Math.random()*ma);
+    let element = m[j].parentElement.parentElement.parentElement;
+    if (element.getAttribute('opacity') == 1) {
+      element.setAttribute('opacity', '0');
+    } else {
+      element.setAttribute('opacity', '1');
+    }
+    
+  }
+}
+
+// console.log(m[6].parentElement.parentElement.parentElement.getAttribute('opacity'))
+
+document.addEventListener('load', init())
+
+function init() {
+  setInterval(() => {
+    sfarfallio();
+  }, 1000);
+}
